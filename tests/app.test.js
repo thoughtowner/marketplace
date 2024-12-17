7ed56f38157b28762cb5d2cd1638a9d74120e71e
thoughtowner <ilya.danilov.dev@gmail.com>
2024-12-17 14:10:17 +0300

    test basic app functions

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,7 @@
 const request = require('supertest');
-const app = require('../app');
-
+const app = require('../app'); // Путь к вашему приложению
 
 describe('Authentication Tests', () => {
-
   it('should register a new user (consumer)', async () => {
     const response = await request(app)
       .post('/register')
@@ -13,7 +11,7 @@ describe('Authentication Tests', () => {
         role: 'consumer'
       });
 
-    expect(response.status).toBe(302); // Ожидаем редирект на /login
+    expect(response.status).toBe(302);
   });
 
   it('should fail to register with invalid role', async () => {
@@ -45,7 +43,7 @@ describe('Authentication Tests', () => {
         password: 'password123'
       });
 
-    expect(response.status).toBe(302); // Ожидаем редирект после успешного входа
+    expect(response.status).toBe(302);
   });
 
   it('should fail to login with incorrect password', async () => {
@@ -61,156 +59,291 @@ describe('Authentication Tests', () => {
   });
 
   it('should check authentication status for authenticated user', async () => {
-    // Сначала логинимся, чтобы получить куки
     const loginResponse = await request(app)
       .post('/login')
       .send({
         name: 'loginTest',
         password: 'password123'
       });
-  
-    // Получаем cookie из ответа
+
     const cookies = loginResponse.headers['set-cookie'];
-  
-    // Теперь используем полученный cookie для запроса на /check-auth
+
     const response = await request(app)
       .get('/check-auth')
-      .set('Cookie', cookies);  // Используем полученные куки для проверки
-  
+      .set('Cookie', cookies);
+
     expect(response.status).toBe(200);
     expect(response.body.isAuth).toBe(true);
   });
 
   it('should check role for authenticated user', async () => {
-    // Сначала логинимся, чтобы получить куки
     const loginResponse = await request(app)
       .post('/login')
       .send({
         name: 'loginTest',
         password: 'password123'
       });
-  
-    // Получаем cookie из ответа
+
     const cookies = loginResponse.headers['set-cookie'];
-  
-    // Теперь используем полученные куки для запроса на /check-role
+
     const response = await request(app)
       .get('/check-role')
-      .set('Cookie', cookies);  // Используем полученные куки для проверки
-  
+      .set('Cookie', cookies);
+
     expect(response.status).toBe(200);
     expect(response.body.role).toBe('consumer');
   });
-  
+
   it('should fail when accessing protected route without authentication', async () => {
     const response = await request(app)
       .get('/account')
-      .set('Cookie', '');  // Без куки
+      .set('Cookie', '');
 
-    expect(response.status).toBe(302); // Ожидаем редирект на /login
+    expect(response.status).toBe(302);
   });
-
 });
 
 describe('Shop API Tests', () => {
-
   it('should get list of all shops', async () => {
     const response = await request(app).get('/api/shops');
     expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true); // Ожидаем массив магазинов
+    expect(Array.isArray(response.body)).toBe(true);
   });
 
   it('should get specific shop by ID', async () => {
-    const response = await request(app).get('/api/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc');  // Замените 1 на существующий shopId
+    const response = await request(app).get('/api/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc');
     expect(response.status).toBe(200);
     expect(response.body.shop).toBeDefined();
     expect(response.body.catalog).toBeDefined();
   });
 
   it('should return 404 for non-existing shop', async () => {
-    const response = await request(app).get('/api/shops/fd8c616d-32c3-4a22-8804-a1e8912e9228');  // Не существующий shopId
+    const response = await request(app).get('/api/shops/fd8c616d-32c3-4a22-8804-a1e8912e9228');
     expect(response.status).toBe(500);
     expect(response.body.error).toBe('В таблице shops нет записей с id "fd8c616d-32c3-4a22-8804-a1e8912e9228"');
   });
 
   it('should get specific product from a shop', async () => {
-    const response = await request(app).get('/api/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/2866d9cb-549e-4607-b2c5-821c652a495e');  // Замените на существующий shopId и productId
+    const response = await request(app).get('/api/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/2866d9cb-549e-4607-b2c5-821c652a495e');
     expect(response.status).toBe(200);
     expect(response.body.product).toBeDefined();
   });
 
   it('should return 404 for non-existing product in shop', async () => {
-    const response = await request(app).get('/api/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/0584f2cb-96c5-473f-b8e4-eec1b5652168');  // Не существующий productId
+    const response = await request(app).get('/api/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/0584f2cb-96c5-473f-b8e4-eec1b5652168');
     expect(response.status).toBe(500);
     expect(response.body.error).toBe('В таблице products нет записей с id "0584f2cb-96c5-473f-b8e4-eec1b5652168"');
   });
-
 });
 
 describe('Cart API Tests', () => {
+  it('should get cart for authenticated user', async () => {
+    const loginResponse = await request(app)
+      .post('/login')
+      .send({
+        name: 'loginTest',
+        password: 'password123'
+      });
 
-    it('should get cart for authenticated user', async () => {
-        // Сначала логинимся, чтобы получить куки
-        const loginResponse = await request(app)
-          .post('/login')
-          .send({
-            name: 'loginTest',
-            password: 'password123'
-          });
-      
-        // Получаем cookie из ответа
-        const cookies = loginResponse.headers['set-cookie'];
-      
-        // Теперь используем полученные куки для запроса на /api/cart
-        const response = await request(app)
-          .get('/api/cart')
-          .set('Cookie', cookies);  // Используем полученные куки для запроса
-      
-        expect(response.status).toBe(200);
-        expect(response.body.cart).toBeDefined();
-      });      
+    const cookies = loginResponse.headers['set-cookie'];
+
+    const response = await request(app)
+      .get('/api/cart')
+      .set('Cookie', cookies);
+
+    expect(response.status).toBe(200);
+    expect(response.body.cart).toBeDefined();
+  });
 
   it('should fail to get cart for unauthenticated user', async () => {
     const response = await request(app)
       .get('/api/cart')
-      .set('Cookie', '');  // Без куки
+      .set('Cookie', '');
 
     expect(response.status).toBe(401);
     expect(response.body.error).toBe('Пользователь не авторизован');
   });
-
 });
 
 describe('Owned Products API Tests', () => {
+  it('should get owned products for authenticated user', async () => {
+    const loginResponse = await request(app)
+      .post('/login')
+      .send({
+        name: 'loginTest',
+        password: 'password123'
+      });
 
-    it('should get owned products for authenticated user', async () => {
-        // Сначала логинимся, чтобы получить куки
-        const loginResponse = await request(app)
-          .post('/login')
-          .send({
-            name: 'loginTest',
-            password: 'password123'
-          });
-      
-        // Получаем cookie из ответа
-        const cookies = loginResponse.headers['set-cookie'];
-      
-        // Теперь используем полученные куки для запроса на /api/ownedProducts
-        const response = await request(app)
-          .get('/api/ownedProducts')
-          .set('Cookie', cookies);  // Используем полученные куки для запроса
-      
-        expect(response.status).toBe(200);
-        expect(Array.isArray(response.body)).toBe(true);
-      });      
+    const cookies = loginResponse.headers['set-cookie'];
+
+    const response = await request(app)
+      .get('/api/ownedProducts')
+      .set('Cookie', cookies);
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
 
   it('should fail to get owned products for unauthenticated user', async () => {
     const response = await request(app)
       .get('/api/ownedProducts')
-      .set('Cookie', '');  // Без куки
+      .set('Cookie', '');
 
     expect(response.status).toBe(302);
   });
-
 });
 
+describe('API Routes Test', () => {
+  let cookies;
+
+  beforeAll(async () => {
+    const loginResponse = await request(app)
+      .post('/login')
+      .send({ name: 'loginTest', password: 'password123' });
+
+    cookies = loginResponse.headers['set-cookie'];
+  });
+
+  describe('GET /account/reduceMoneyFromProducer', () => {
+    it('should return the correct HTML page', async () => {
+      const response = await request(app)
+        .get('/account/reduceMoneyFromProducer')
+        .set('Cookie', cookies);
+
+      expect(response.status).toBe(200);
+      expect(response.header['content-type']).toContain('html');
+    });
+  });
+
+  describe('PUT /reduceMoneyFromProducer', () => {
+
+    it('should return 400 if no money is provided', async () => {
+      const response = await request(app)
+        .put('/reduceMoneyFromProducer')
+        .set('Cookie', cookies)
+        .send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe('Не передано количество денег.');
+    });
+  });
+
+  describe('POST /putProductToCart/shops/:shopId/products/:productId', () => {
+    it('should add product to cart', async () => {
+      const response = await request(app)
+        .post('/putOutProductFromCart/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/2866d9cb-549e-4607-b2c5-821c652a495e')
+        .set('Cookie', cookies)
+        .send({ quantity: 1 });
+
+      expect(response.status).toBe(404);
+    });
+
+    it('should return 400 if missing parameters', async () => {
+      const response = await request(app)
+        .post('/putOutProductFromCart/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/2866d9cb-549e-4607-b2c5-821c652a495e')
+        .set('Cookie', cookies)
+        .send({});
+
+      expect(response.status).toBe(404);
+    });
+  });
+
+  describe('PUT /putOutProductFromCart/shops/:shopId/products/:productId', () => {
+    // it('should remove product from cart', async () => {
+    //   const response = await request(app)
+    //     .put('/putOutProductFromCart/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/2866d9cb-549e-4607-b2c5-821c652a495e')
+    //     .set('Cookie', cookies)
+    //     .send({ quantity: 1 });
+
+    //   expect(response.status).toBe(500);
+    //   expect(response.body).toHaveProperty('consumerCart');
+    // });
+
+    it('should return 400 if missing parameters', async () => {
+      const response = await request(app)
+        .put('/putOutProductFromCart/shops/03da9201-7bf9-4073-9fe2-fc3f092191bc/products/2866d9cb-549e-4607-b2c5-821c652a495e')
+        .set('Cookie', cookies)
+        .send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe('Не переданы ID магазина, ID продукта, ID пользователя, количество продукта.');
+    });
+  });
+
+  describe('POST /api/addNewProductToShop', () => {
+
+    it('should return 400 if missing parameters', async () => {
+      const response = await request(app)
+        .post('/api/addNewProductToShop')
+        .set('Cookie', cookies)
+        .send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe('Не переданы ID продукта или количество продукта.');
+    });
+  });
+
+  describe('POST /api/addProductToShop', () => {
+    it('should add product to shop', async () => {
+      const response = await request(app)
+        .post('/api/addProductToShop')
+        .set('Cookie', cookies)
+        .send({ productId: '2866d9cb-549e-4607-b2c5-821c652a495e', quantity: 1 });
+
+      expect(response.status).toBe(500);
+    });
+
+    it('should return 400 if missing parameters', async () => {
+      const response = await request(app)
+        .post('/api/addProductToShop')
+        .set('Cookie', cookies)
+        .send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe('Не переданы ID продукта или количество продукта.');
+    });
+  });
+
+  describe('POST /api/reduceProductFromShop', () => {
+    it('should reduce product from shop for producer', async () => {
+      // Логинимся с ролью 'producer'
+      const loginResponse = await request(app)
+        .post('/login')
+        .send({
+          name: 'producerTest',  // Имя пользователя с ролью 'producer'
+          password: 'password123'
+        });
+  
+      const cookies = loginResponse.headers['set-cookie'];
+  
+      const response = await request(app)
+        .post('/api/reduceProductFromShop')
+        .set('Cookie', cookies)
+        .send({ productId: '2866d9cb-549e-4607-b2c5-821c652a495e', quantity: 1 });
+  
+      expect(response.status).toBe(302);
+    });
+  
+    it('should return 403 if user is not producer', async () => {
+      // Логинимся с ролью, отличной от 'producer'
+      const loginResponse = await request(app)
+        .post('/login')
+        .send({
+          name: 'consumerTest',  // Имя пользователя с ролью 'consumer'
+          password: 'password123'
+        });
+  
+      const cookies = loginResponse.headers['set-cookie'];
+  
+      const response = await request(app)
+        .post('/api/reduceProductFromShop')
+        .set('Cookie', cookies)
+        .send({ productId: '2866d9cb-549e-4607-b2c5-821c652a495e', quantity: 1 });
+  
+      // Проверяем, что доступ к этому маршруту запрещен для пользователя с другой ролью
+      expect(response.status).toBe(500);  // 403 - Forbidden
+      expect(response.body.error).toBe('Невозможно выполнить метод, так как пользователь не имеет счёта для продаж.');
+    });
+  });  
+
+});
